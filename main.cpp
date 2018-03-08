c74cf1fe978c193fb89f9fef7c9c3237a4e204a7
dirtdiver353 <dirtdiver353@gmail.com>
2018-03-08 23:05:51 +0100

    prueba movimiento

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-#define kVel 5
+#define kVel 3
 
 int main()
 {
@@ -23,9 +23,10 @@ int main()
     sprite.setTextureRect(sf::IntRect(0*64, 0*64, 64, 64));
     // Lo dispongo en su posicion en la pantalla
     sprite.setPosition(320, 240);
-
     
-
+    int xd = 0, yd = 0, xu = 0, ys = 0;
+    int yu = 4, xs = 4;
+    bool bs = false, bu = false, bd = false;
     //Bucle del juego
     while (window.isOpen())
     {
@@ -49,26 +50,52 @@ int main()
                         
                         //Mapeo del cursor
                         case sf::Keyboard::Right:
-                            sprite.setTextureRect(sf::IntRect(4*64, 4*64, 64, 64));
-                            //Escala por defecto
+                            if(bs == true){
+                                if(xs < 8) xs++;
+                                if(ys < 4) ys++;
+                                if(xs > 7) xs = 4;
+                                if(ys > 3) ys = 0;
+                                bs = false;
+                            }else{bs = true;}
+                            sprite.setTextureRect(sf::IntRect(xs*64, ys*64, 64, 64));
                             sprite.setScale(-1,1);
                             sprite.move(kVel,0);
                         break;
 
                         case sf::Keyboard::Left:
-                            sprite.setTextureRect(sf::IntRect(4*64, 4*64, 64, 64));
-                            //Reflejo vertical
+                                if(bs == true){
+                                if(xs < 8) xs++;
+                                if(ys < 4) ys++;
+                                if(xs > 7) xs = 4;
+                                if(ys > 3) ys = 0;
+                            bs = false;
+                            }else{bs = true;}
+                            sprite.setTextureRect(sf::IntRect(xs*64, ys*64, 64, 64));
                             sprite.setScale(1,1);
                             sprite.move(-kVel,0); 
                         break;
                         
                         case sf::Keyboard::Up:
-                            sprite.setTextureRect(sf::IntRect(0*64, 4*64, 64, 64));
+                                if(bu == true){
+                                if(xu < 4) xu++;
+                                if(yu < 8) yu++;
+                                if(xu > 3) xu = 0;
+                                if(yu > 7) yu = 4;
+                                bu = false;
+                            }else{bu = true;}
+                            sprite.setTextureRect(sf::IntRect(xu*64, yu*64, 64, 64));
                             sprite.move(0,-kVel); 
                         break;
                         
                         case sf::Keyboard::Down:
-                            sprite.setTextureRect(sf::IntRect(0*64, 0*64, 64, 64));
+                            if(bd == true){
+                                if(xd < 4) xd++;
+                                if(yd < 4) yd++; 
+                                if(xd > 3) xd = 0;
+                                if(yd > 3) yd = 0;
+                                bd = false;
+                            }else{bd = true;}
+                            sprite.setTextureRect(sf::IntRect(xd*64, yd*64, 64, 64));
                             sprite.move(0,kVel); 
                         break;
                         
